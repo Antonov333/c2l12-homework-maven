eb9c2fe5febf3338c6ff6f5d59adf31385bac3fe
Antonov333 <antonovsergei333@yahoo.com>
2023-06-13 13:29:52 +0300

    Connected StringUtils Library

---
+++ src/main/java/pro/sky/course2/Main.java
@@ -0,0 +1,15 @@
+package pro.sky.course2;
+
+import org.apache.commons.lang3.StringUtils;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println(StringUtils.capitalize("hi! this is my homework for libraries topic"));
+
+
+    }
+}
